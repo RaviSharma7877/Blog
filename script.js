f82c64a051d9c7dc822e3bcc4817415ae18f43bc
RaviSharma7877 <sraa7877@gmail.com>
2023-06-29 21:32:31 +0530

    done

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const search = document.querySelector('.search');
 icon.onclick = function(){
     search.classList.toggle('active');
 }
+
+let icon1 = document.getElementById('icon');
+icon1.onclick = function(){
+    document.body.classList.toggle('darkthem');
+    if(document.body.classList.contains('darkthem')){
+        icon1.src = "/images/sun.png";
+    }else{
+        icon1.src = "/images/moon.png";
+    }
+}
